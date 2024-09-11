b3d32b0088fbeef5197b7d70925bc82e63bb3cbb
Marco Aurélio <marco@crabki.com>
2024-09-11 14:38:27 -0300

    :sparkles: Adicionar componentes da UI

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -1,6 +1,7 @@
 import logo from './assets/logo-in-orbit.svg'
 import letsStart from './assets/lets-start-illustration.svg'
 import { Plus } from 'lucide-react'
+import { Button } from './components/ui/button'
 export function App() {
   return (
     <div className="h-screen flex flex-col items-center justify-center gap-8">
@@ -9,13 +10,10 @@ export function App() {
       <p className="text-zinc-300 leading-relaxed max-w-80 text-center">
         Você ainda não cadastrou nenhuma meta, que tal cadastrar um agora mesmo?
       </p>
-      <button
-        type="button"
-        className="text-sm font-medium tracking-tight px-4 py-2.5 rounded-lg bg-violet-500 text-violet-50 flex items-center gap-2 hover:bg-violet-600 transition-all"
-      >
+      <Button>
         <Plus className="size-4" />
         Cadastrar meta
-      </button>
+      </Button>
     </div>
   )
 }
